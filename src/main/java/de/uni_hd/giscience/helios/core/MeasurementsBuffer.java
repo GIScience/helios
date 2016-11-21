984b905161293dccc43c3f1c61fa73945729f3f5
KoeMai <github@mairbaeurl.de>
2016-11-21 22:01:41 +0100

    Bugfix: The method getLastRecordedPointIndex provided a invalid value when the buffer was overflowed. This implementation makes the calculation of the next index on add. So that the getLastRecordPointIndex only provide the index.

--- src/main/java/de/uni_hd/giscience/helios/core/MeasurementsBuffer.java
+++ src/main/java/de/uni_hd/giscience/helios/core/MeasurementsBuffer.java
@@ -2,37 +2,57 @@ package de.uni_hd.giscience.helios.core;
 
 import de.uni_hd.giscience.helios.core.scanner.Measurement;
 
+/**
+ * Buffer for measurement values which clears the buffer on overflow.
+ * This is a basic version of a ring buffer. It has no collision detection on read/write overflow.
+ */
 public class MeasurementsBuffer {
 
 	// Measurements buffer:
 	final private static int MEASUREMENTS_BUFFER_SIZE = 100000;
 	Measurement[] buffer = new Measurement[MEASUREMENTS_BUFFER_SIZE];
-	int nextInsertIndex = 1;
+	int lastWrittenIndex = 0;
 
+	/**
+	 * Adds a measurement, on overflow the measurement is added to the first entry of the buffer
+	 * @param m measurement to add
+	 */
 	public synchronized void add(Measurement m) {
 
-		// ############# BEGIN Add measurement to measurements buffer ############
-		buffer[nextInsertIndex] = m;
+        lastWrittenIndex++;
 
-		nextInsertIndex++;
+        if (lastWrittenIndex >= MEASUREMENTS_BUFFER_SIZE) {
+            lastWrittenIndex = 0;
+        }
 
-		if (nextInsertIndex >= buffer.length - 1) {
-			nextInsertIndex = 1;
-		}
-		// ############# END Add measurement to measurements buffer ############
+		buffer[lastWrittenIndex] = m;
 	}
 
+    /**
+     * Return measurment from index
+     * @param index Position of measurement
+     * @return measurement on index
+     */
 	public Measurement getEntryAt(int index) {
 		return buffer[index];
 	}
 
+	/**
+	 * Returns the index of the last appended measurement
+	 * @return index of last entry
+	 *
+	 */
 	public int getLastRecordedPointIndex() {
-		return nextInsertIndex - 1;
+		return lastWrittenIndex;
+
 	}
 
-	
+	/**
+	 * Returns the maximum count of measurements
+	 * @return capacity of buffer
+	 */
 	public int getSize() {
-		return buffer.length;
+		return MEASUREMENTS_BUFFER_SIZE;
 	}
 	
 }
