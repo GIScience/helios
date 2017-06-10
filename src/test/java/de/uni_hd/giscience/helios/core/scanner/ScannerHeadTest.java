36a3364bcef8864581169e888f879fcd9e66459f
KoeMai <github@mairbaeurl.de>
2017-06-10 21:46:48 +0200

    Added test setup for unit testing.

---
+++ src/test/java/de/uni_hd/giscience/helios/core/scanner/ScannerHeadTest.java
@@ -0,0 +1,14 @@
+package de.uni_hd.giscience.helios.core.scanner;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by matthias on 10.06.17.
+ */
+public class ScannerHeadTest {
+  @Test
+  void myFirstTest() {
+    assertEquals(2, 1 + 1);
+  }
+}
